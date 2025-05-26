044c636f63d4962746cc852a15812f7a69d3ec43
Elendu Franklin <elendufranklin66@gmail.com>
2025-05-26 11:13:57 +0100

    added descriptive forbidden error message

--- src/auth/guard/roles.guard.ts
+++ src/auth/guard/roles.guard.ts
@@ -1,4 +1,4 @@
-import { Injectable, CanActivate, ExecutionContext } from '@nestjs/common';
+import { Injectable, CanActivate, ExecutionContext, ForbiddenException } from '@nestjs/common';
 import { Reflector } from '@nestjs/core';
 
 @Injectable()
@@ -11,7 +11,13 @@ export class RolesGuard implements CanActivate {
       context.getClass(),
     ]);
     if (!requiredRoles) return true;
+    
     const { user } = context.switchToHttp().getRequest();
+
+    if (!requiredRoles.includes(user.role)) {
+      throw new ForbiddenException('You do not have permission');
+    }
+
     return requiredRoles.includes(user.role);
   }
 }
